48db2cda758b93f195b52df306de428db131b13a
premiumfriedrice <lloyddalba@gmail.com>
2023-11-07 21:26:31 -0600

    created basic form layout and functionality for balance sheet. Only works for expense side (right) so far. Need to modify bs.js to have both sides be responsive.

---
+++ static/bs.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('expense-form');
+    const addButton = form.querySelector('.add-button');
+
+    addButton.addEventListener('click', function() {
+        const formInput = form.querySelector('.form-input').cloneNode(true);
+        formInput.querySelector('input[name="name"]').value = '';
+        formInput.querySelector('input[name="amount"]').value = '';
+        formInput.querySelector('select[name="frequency"]').selectedIndex = 0;
+
+        const deleteButton = formInput.querySelector('.delete-button');
+        deleteButton.addEventListener('click', function() {
+            form.removeChild(formInput);
+        });
+
+        form.insertBefore(formInput, addButton);
+    });
+});
